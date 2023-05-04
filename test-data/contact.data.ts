b54552cf3b00c3d6839089ae8b3c015542472525
Elzbieta Wilk <121565930+ElzbietaZofia@users.noreply.github.com>
2023-05-04 16:45:57 +0200

    Add files via upload

---
+++ test-data/contact.data.ts
@@ -0,0 +1,8 @@
+export const contactFormData = {
+
+    name: 'John',
+    surname: 'Travolta',
+    email: 'mecaxa4624@soombo.com',
+    question: 'At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'
+  }
+  
